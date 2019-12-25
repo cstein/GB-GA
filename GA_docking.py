e7433a8353003a4c516b9f895b829a107ebb61c1
Casper Steinmann <casper.steinmann@gmail.com>
2019-12-25 20:06:01 +0100

    Adds new random seed pr. iteration.

--- GA_docking.py
+++ GA_docking.py
@@ -51,6 +51,7 @@ ga_settings.add_argument("--ncpu", metavar="number", type=int, default=n_cpus, h
 ga_settings.add_argument("--nconf", metavar="number", type=int, default=n_confs, help="number of conformations per ligand to sample. Default: %(default)s.")
 ga_settings.add_argument("--maxscore", metavar="float", type=float, default=max_score, help="The maximum value of the property. Default: %(default)s.")
 ga_settings.add_argument("--randint", metavar="number", type=int, default=-1, help="Specify a positive integer as random seed. Any other values will sample a number from random distribution. Default: %(default)s")
+ga_settings.add_argument("--no-prune", default=prune_population, action="store_false", help="No pruning of mating pools and generations.")
 
 glide_settings = ap.add_argument_group("Glide Settings")
 glide_settings.add_argument("--glide-grid", metavar="grid", type=str, default="", action=ExpandPath, help="Path to docking grid.")
@@ -71,9 +72,9 @@ n_cpus = args.ncpu
 n_confs = args.nconf
 max_score = args.maxscore
 if args.randint > 0:
-    random_seed = args.randint
+    random_seeds = [args.randint for i in range(n_tries)]
 else:
-    random_seed = random.randint(1, 100000)
+    random_seeds = numpy.random.randint(100000, size=n_tries)
 
 basename = args.basename
 if len(basename) > 0:
@@ -105,14 +106,14 @@ print('* average_size/size_stdev', co.average_size, co.size_stdev)
 print('* initial pool', smiles_filename)
 print('* number of tries', n_tries)
 print('* number of CPUs', n_cpus)
-print('* seed', random_seed)
+print('* seed(s)', random_seeds)
 print('* basename', basename)
 print('*** GLIDE SETTINGS ***')
 print('* grid', glide_grid)
 print('* precision', glide_precision)
 print('* method', glide_method)
 print('* ')
-print('run,score,smiles,generations,prune')
+print('run,score,smiles,generations,prune,seed')
 
 
 def GA(args):
@@ -139,8 +140,6 @@ def GA(args):
         print("  >> Generation {0:3d} finished in {1:.1f} min <<".format(generation+1, (t2_gen-t1_gen)/60.0))
         if scores[0] >= max_score:
             break
-    else:
-        generation = 0
 
     return scores, population, high_scores, generation+1
 
@@ -151,17 +150,16 @@ t0 = time.time()
 all_scores = []
 
 high_scores_list = []
-for prune_population in [True, False]:
-    for i in range(n_tries):
-        (final_scores, final_population, final_hs, final_generation) = GA([population_size, smiles_filename, scoring_function, generations, mating_pool_size, mutation_rate, scoring_args, max_score, prune_population, random_seed])
-        all_scores.append(final_scores)
-        smiles = Chem.MolToSmiles(final_population[0], isomericSmiles=True)
-        results.append(final_scores[0])
-        high_scores_list.append(final_hs)
-        print("{0:d},{1:.2f},{2:s},{3:d},{4:}".format(i, final_scores[0], smiles, final_generation, prune_population))
+for i, random_seed in zip(range(n_tries), random_seeds):
+    (final_scores, final_population, final_hs, final_generation) = GA([population_size, smiles_filename, scoring_function, generations, mating_pool_size, mutation_rate, scoring_args, max_score, prune_population, random_seed])
+    all_scores.append(final_scores)
+    smiles = Chem.MolToSmiles(final_population[0], isomericSmiles=True)
+    results.append(final_scores[0])
+    high_scores_list.append(final_hs)
+    print("{0:d},{1:.2f},{2:s},{3:d},{4:},{5:d}".format(i, final_scores[0], smiles, final_generation, prune_population,random_seed))
 
-        # dump after each generation, overwrite old file if things crash.
-        pickle.dump(high_scores_list, open(basename+'out.b', 'wb' ))
+    # dump after each generation, overwrite old file if things crash.
+    pickle.dump(high_scores_list, open('GA_dock_' + basename+'.npy', 'wb' ))
 
 t1 = time.time()
 print("")
