5cfb3bfe31b820711856e6173f5aeaaef1e0ac9a
Casper Steinmann <casper.steinmann@gmail.com>
2021-03-05 13:35:24 +0100

    Adds faster docking with smina

--- docking/smina_dock.in.sh
+++ docking/smina_dock.in.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export SMINAPATH="$SMINA"
 export SMINAEXE="$EXE"
-"${SMINAPATH}/${SMINAEXE}" -r ${RECEPTOR} -l ${LIGAND} --num_modes=${NUMMODES} --center_x ${CX} --center_y ${CY} --center_z ${CZ} --size_x ${BOXSIZE} --size_y ${BOXSIZE} --size_z ${BOXSIZE} --cpu ${NCPUS} -o ${BASENAME}.pdbqt --log ${BASENAME}.log
+"${SMINAPATH}/${SMINAEXE}" -r ${RECEPTOR} -l ${LIGAND} --num_modes=${NUMMODES} --exhaustiveness 1 --center_x ${CX} --center_y ${CY} --center_z ${CZ} --size_x ${BOXSIZE} --size_y ${BOXSIZE} --size_z ${BOXSIZE} --cpu ${NCPUS} -o ${BASENAME}.pdbqt --log ${BASENAME}.log
 exit
